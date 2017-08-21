90443e2bcb570afa182b32a064449170614a3e8e
Ellon Daris <17701465649@163.com>
2017-08-22 02:15:56 +0800

    update views.py

--- flask-todo/app/views.py
+++ flask-todo/app/views.py
@@ -5,7 +5,13 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    return render_template("index.html",text="GYY")
+    form = TodoForm()
+    todos = Todo.objects.order_by('-time')
+    return render_template("index.html",todos=todos, form=form)
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html'),404
 
 if __name__ == '__main__':
     app.run()
