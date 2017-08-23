4e92f14a285f847b32cad35e4406eb97372850e2
Ellon Daris <17701465649@163.com>
2017-08-23 23:11:17 +0800

    sqlite config

--- flask-todo/app/__init__.py
+++ flask-todo/app/__init__.py
@@ -1,6 +1,16 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
 
 app = Flask(__name__)
 app.config.from_object("config")
+app.config('SQLALCHEMY_DATABASE_URI') =\
+	'sqlite:///' + os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True 
+
+db = SQLAlchemy(app)
 
 from app import views, models
