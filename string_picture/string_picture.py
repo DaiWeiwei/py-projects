6b8edca36c2273ed58a532610f0ce27c978c6c90
Ellon Daris <17701465649@163.com>
2017-10-10 23:59:00 +0800

    v.0.1

--- string_picture/string_picture.py
+++ string_picture/string_picture.py
@@ -1,9 +1,11 @@
 from PIL import Image
-
+import os
 
 ascii_char = list(r"$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ")
+pic = os.path.join(os.path.dirname(__file__), 'jobs.jpeg')
+text = os.path.join(os.path.dirname(__file__), 'jobs.txt')
 
-def get_char(r,g,b,alpha):
+def get_char(r,g,b,alpha = 256):
 	if alpha == 256:
 		return ' '
 
@@ -13,3 +15,19 @@ def get_char(r,g,b,alpha):
 	unit = (256+1)/length
 
 	return ascii_char[int(gray / unit)]
+
+if __name__ == '__main__':
+	im = Image.open(pic)
+	im = im.resize((80, 80), Image.NEAREST)
+
+	txt = ''
+
+	for i in range(80):
+		for j in range(80):
+			txt += get_char(*im.getpixel((j,i)))
+			txt += '\n'
+
+	print(txt)
+
+	with open(text ,'w') as f:
+		f.write(txt)
