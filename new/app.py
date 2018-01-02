c9f8afd5d46b57df71dab0e385b24bc8370b8d76
Ellon Daris <17701465649@163.com>
2018-01-02 23:49:57 +0800

    start 0.1

--- new/app.py
+++ new/app.py
@@ -1,5 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 
-app = Flask(__file__)
+app = Flask(__name__)
 bootstrap = Bootstrap(app)
+
+@app.route("/<name>")
+def index(name):
+	return render_template("user.html", name=name)
+
+
+if __name__ == '__main__':
+	app.run()
