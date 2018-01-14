32964a56cd05ade6a6db0aca912defb1f38b65b3
Ellon Daris <17701465649@163.com>
2018-01-14 23:49:16 +0800

    mysql notes

--- new/mysql.sql
+++ new/mysql.sql
@@ -10,3 +10,29 @@ create_definition:
   DEFAULT default_value
   auto_increament
   PRIMARY KEY
+
+ALTER [ignore] TABLE 表名 alter_spec[,alter_spec]...
+当指定ignore时，如果出现重复关键的行，则只执行一行，其他重复的行被删除。
+
+alter_specification:
+  ADD [COLUMN] create_difinition [FIRST|after column_name]
+  ADD index [index_name](index_col_name,...)
+  ADD PRIMARY KEY (index_col_name,...)
+  ADD  UNIQUE [index_name](index_col_name,...)
+  -- ALTER [COLUMN] col_name {SET DEFAULT literral|DROP DEFAULT}
+  change [COLUMN] old_col_name create_definition
+  modify [COLUMN] create_definition
+  DROP [COLUMN] col_name
+  DROP PRIMARY KEY
+  DROP index index_name
+  rename [AS] new_tbl_name
+  table_options
+
+RENAME TABLE old to new
+
+DROP TABLE if exists  表名
+
+INSERT INTO table(col1,col2) VALUES (val1, val2)
+
+
+
