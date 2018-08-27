14b514aa67a8df3815f900945850029c28ceb49f
DaiWeiwei <17701465649@163.com>
2018-08-27 15:54:48 +0800

    validator

--- webAPP/app/web/book.py
+++ webAPP/app/web/book.py
@@ -1,5 +1,6 @@
 from flask import jsonify,request
 
+from app.forms.book import SearchForm
 from . import web
 from controller import Book
 from helper import is_isbn_or_key
@@ -7,11 +8,17 @@ from helper import is_isbn_or_key
 
 @web.route('/book/search')
 def search():
-	q = request.args.get('q')
-	isbn_or_key = is_isbn_or_key(q)
-	if isbn_or_key == 'isbn':
-		result = Book.search_by_isbn(q)
+	# q = request.args.get('q')
+	form = SearchForm(request.args)
+	if form.validate():
+		q = form.q.data.strip()
+		page = form.page.data
+		isbn_or_key = is_isbn_or_key(q)
+		if isbn_or_key == 'isbn':
+			result = Book.search_by_isbn(q)
+		else:
+			result = Book.search_by_keyword(q)
+		return jsonify(result)
+		# return json.dumps(result),200,{'constent-type':'application/json'}
 	else:
-		result = Book.search_by_keyword(q)
-	return jsonify(result)
-	# return json.dumps(result),200,{'constent-type':'application/json'}
+		return jsonify({"msg":"参数校验失败"})
